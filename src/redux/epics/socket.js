c72e6947f93a4025f0911025a3f4613e42b7c496
Yangjiaxi <1468764755@qq.com>
2019-10-19 17:01:42 +0800

    <MOD> fine tune

--- src/redux/epics/socket.js
+++ src/redux/epics/socket.js
@@ -27,10 +27,19 @@ const socketConnectEpic = (action$, state$, { io, socket$ }) =>
     switchMap(
       ({ id }) =>
         new Observable(o => {
+          const {
+            user: { token },
+          } = state$.value;
           const socket = io(API, {
-            query: { id },
+            query: { id, token },
+            forceNew: true,
           });
-          socket.on("connect", () => o.next(socket));
+          socket.on("connect", () => {
+            if (socket$.getValue()) {
+              socket$.getValue().close();
+            }
+            return o.next(socket);
+          });
         }),
     ),
     tap(socket$),
@@ -53,28 +62,24 @@ const socketEventEpic = (action$, state$, { socket$ }) =>
       !socket
         ? EMPTY
         : new Observable(o => {
-          // disconnected by accident
-          socket.on("disconnect", () => {
-            o.next(
-              enqueueSnackbar(i18nHelper.disconnected, { variant: "info" }),
-            );
-          });
-          // trying
-          socket.on("reconnecting", () => {
-            o.next(
-              enqueueSnackbar(i18nHelper.reconnectAttempt, {
-                variant: "info",
-              }),
-            );
-          });
-          // success
-          socket.on("reconnect", () => {
-            const {
-              content: { id },
-            } = state$.value;
-            o.next(checkoutContentStart(id));
-          });
-        }),
+            // disconnected by accident
+            socket.on("disconnect", () => {});
+            // trying
+            socket.on("reconnecting", () => {
+              o.next(
+                enqueueSnackbar(i18nHelper.reconnectAttempt, {
+                  variant: "info",
+                }),
+              );
+            });
+            // success
+            socket.on("reconnect", () => {
+              const {
+                content: { id },
+              } = state$.value;
+              o.next(checkoutContentStart(id));
+            });
+          }),
     ),
   );
 
@@ -84,91 +89,91 @@ const socketReceiveEpic = (action$, state$, { socket$ }) =>
       !socket
         ? EMPTY
         : new Observable(o => {
-          socket.on("connected", () => {
-            o.next(
-              enqueueSnackbar(i18nHelper.socketConnected, {
-                variant: "success",
-              }),
-            );
-          });
-          // change title
-          socket.on("changeTitle", ({ content }) => {
-            o.next(toggleProgress(true));
-            o.next(changeTitleFinish(content));
-            o.next(toggleProgress());
-          });
-          socket.on("changeTitleError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // append floor
-          socket.on("appendFloor", ({ time, content, id }) => {
-            o.next(toggleProgress(true));
-            o.next(appendFloorFinish(id, time, content));
-            o.next(toggleProgress());
-          });
-          socket.on("appendFloorError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // change floor
-          socket.on("changeFloor", ({ time, content, id }) => {
-            o.next(toggleProgress(true));
-            o.next(changeFloorFinish(id, time, content));
-            o.next(toggleProgress());
-          });
-          socket.on("changeFloorError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // delete floor
-          socket.on("deleteFloor", ({ id }) => {
-            o.next(toggleProgress(true));
-            o.next(deleteFloorFinish(id));
-            o.next(toggleProgress());
-          });
-          socket.on("deleteFloorError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // move floor
-          socket.on("moveFloor", ({ from, to }) => {
-            o.next(toggleProgress(true));
-            o.next(moveFloorFinish(from, to));
-            o.next(toggleProgress());
-          });
-          socket.on("moveFloorError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // add comment
-          socket.on("addComment", ({ floorID }) => {
-            o.next(toggleProgress(true));
-            o.next(addCommentFinish(floorID));
-            o.next(toggleProgress());
-          });
-          socket.on("addCommentError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-          // vote comment
-          socket.on("voteComment", ({ floorID }) => {
-            o.next(toggleProgress(true));
-            o.next(voteCommentFinish(floorID));
-            o.next(toggleProgress());
-          });
-          socket.on("voteCommentError", res => {
-            const { message, type } = res;
-            o.next(enqueueSnackbar(message, { variant: type || "error" }));
-            o.next(toggleProgress());
-          });
-        }),
+            socket.on("connected", () => {
+              o.next(
+                enqueueSnackbar(i18nHelper.socketConnected, {
+                  variant: "success",
+                }),
+              );
+            });
+            // change title
+            socket.on("changeTitle", ({ content }) => {
+              o.next(toggleProgress(true));
+              o.next(changeTitleFinish(content));
+              o.next(toggleProgress());
+            });
+            socket.on("changeTitleError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // append floor
+            socket.on("appendFloor", ({ time, content, id }) => {
+              o.next(toggleProgress(true));
+              o.next(appendFloorFinish(id, time, content));
+              o.next(toggleProgress());
+            });
+            socket.on("appendFloorError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // change floor
+            socket.on("changeFloor", ({ time, content, id }) => {
+              o.next(toggleProgress(true));
+              o.next(changeFloorFinish(id, time, content));
+              o.next(toggleProgress());
+            });
+            socket.on("changeFloorError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // delete floor
+            socket.on("deleteFloor", ({ id }) => {
+              o.next(toggleProgress(true));
+              o.next(deleteFloorFinish(id));
+              o.next(toggleProgress());
+            });
+            socket.on("deleteFloorError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // move floor
+            socket.on("moveFloor", ({ from, to }) => {
+              o.next(toggleProgress(true));
+              o.next(moveFloorFinish(from, to));
+              o.next(toggleProgress());
+            });
+            socket.on("moveFloorError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // add comment
+            socket.on("addComment", ({ floorID }) => {
+              o.next(toggleProgress(true));
+              o.next(addCommentFinish(floorID));
+              o.next(toggleProgress());
+            });
+            socket.on("addCommentError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+            // vote comment
+            socket.on("voteComment", ({ floorID }) => {
+              o.next(toggleProgress(true));
+              o.next(voteCommentFinish(floorID));
+              o.next(toggleProgress());
+            });
+            socket.on("voteCommentError", res => {
+              const { message, type } = res;
+              o.next(enqueueSnackbar(message, { variant: type || "error" }));
+              o.next(toggleProgress());
+            });
+          }),
     ),
   );
 
